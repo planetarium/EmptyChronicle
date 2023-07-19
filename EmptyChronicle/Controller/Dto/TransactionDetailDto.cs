4e82b7a77ef5e3916e8b772395b0aec6e2c59518
XxshiftxX <qjfrntop12@gmail.com>
2023-07-19 11:33:52 +0900

    Fix nullable warning on DTO

--- EmptyChronicle/Controller/Dto/TransactionDetailDto.cs
+++ EmptyChronicle/Controller/Dto/TransactionDetailDto.cs
@@ -4,25 +4,25 @@ namespace EmptyChronicle.Controller.Dto;
 
 public class TransactionDetailDto
 {
-    public string Signature { get; init; }
+    public string? Signature { get; init; }
 
-    public string Signer { get; init; }
+    public string? Signer { get; init; }
 
-    public string PublicKey { get; init; }
+    public string? PublicKey { get; init; }
 
     public DateTimeOffset UpdateTime { get; init; }
 
     public DateTimeOffset Timestamp { get; init; }
 
-    public string Status { get; init; }
+    public string? Status { get; init; }
 
     public long Nonce { get; init; }
 
     public long BlockIndex { get; init; }
 
-    public string Id { get; init; }
+    public string? Id { get; init; }
 
-    public JsonNode[] Actions { get; init; }
+    public JsonNode[] Actions { get; init; } = Array.Empty<JsonNode>();
 
-    public string[] UpdatedAddresses { get; init; }
+    public string[] UpdatedAddresses { get; init; } = Array.Empty<string>();
 }
--- EmptyChronicle/Controller/Dto/TransactionDto.cs
+++ EmptyChronicle/Controller/Dto/TransactionDto.cs
@@ -5,13 +5,13 @@ namespace EmptyChronicle.Controller.Dto;
 
 public class TransactionDto
 {
-    public long BlockIndex { get; set; }
-    public string Signature { get; set; }
-    public string[] UpdatedAddresses { get; set; }
-    public string Id { get; set; }
-    public string PublicKey { get; set; }
-    public JsonNode[] Actions { get; set; }
-    public long Nonce { get; set; }
-    public string Signer { get; set; }
-    public DateTimeOffset Timestamp { get; set; }
+    public long BlockIndex { get; init; }
+    public string? Signature { get; init; }
+    public string[]? UpdatedAddresses { get; init; }
+    public string? Id { get; init; }
+    public string? PublicKey { get; init; }
+    public JsonNode[]? Actions { get; init; }
+    public long Nonce { get; init; }
+    public string? Signer { get; init; }
+    public DateTimeOffset Timestamp { get; init; }
 }
